5188e545e87559512a01c5923af0d42fc4280752
andy grosser <dioptre@gmail.com>
2018-01-09 15:03:37 +0100

    updates for IE,edge safari

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,8 @@ module.exports = {
         exclude: /(node_modules)/,
         loader: 'babel-loader',
         query: {
-          presets: ['es2015']
+          presets: ['es2015'],
+          plugins: [require('babel-plugin-async-to-promises')]          
         }
       }
     ]
